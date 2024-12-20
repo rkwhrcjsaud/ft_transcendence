464ea6347e2cfd6252cef41c6f67052aef14a453
김기범 <gimgibeom@gimgibeom-ui-MacBookAir.local>
2024-12-20 18:39:24 +0900

    [Fix/#65] css파일 이름 오류 수정,front를 js로 이식, make를 통해 빌드가능

--- back/friends/models.py
+++ back/friends/models.py
@@ -1,9 +1,9 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Friend(models.Model):
-    user = models.ForeignKey(User, related_name="friends", on_delete=models.CASCADE)
-    friend = models.ForeignKey(User, related_name="friend_of", on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="friends", on_delete=models.CASCADE)
+    friend = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="friend_of", on_delete=models.CASCADE)
     create_at = models.DateTimeField(auto_now_add=True)
 
     class Meta:
--- front/vite.config.js
+++ front/vite.config.js
@@ -1,5 +1,9 @@
 // vite.config.js
 export default {
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+    },
     css: {
         preprocessorOptions: {
             scss: {

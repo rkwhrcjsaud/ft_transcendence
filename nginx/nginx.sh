0686a478ebf7811e617f384dcb7b2ae38f848a8e
J-millar99 <wlwogus33@gamil.com>
2025-01-06 16:20:07 +0900

    fix/#138 imac 적용작업

--- nginx/nginx.sh
+++ nginx/nginx.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+until nc -z django 8000; do
+    sleep 1
+done
+
+
+
 openssl req -newkey rsa:4096 -x509 -sha256 -days 365 -nodes \
        -out /etc/ssl/certs/certs.crt \
        -keyout /etc/ssl/private/certs.key \

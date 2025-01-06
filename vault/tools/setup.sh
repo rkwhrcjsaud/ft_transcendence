20400863c9eb9b65ab0f9865be1441cd2035e5b9
J-millar99 <wlwogus33@gamil.com>
2025-01-06 18:30:07 +0900

    fix/#146 vault 조회 안정성 보장

--- vault/tools/setup.sh
+++ vault/tools/setup.sh
@@ -29,17 +29,11 @@ vault operator unseal $(cat $VAULT_UNSEAL_KEY)
 # Vault 로그인
 vault login $(cat $VAULT_ROOT_TOKEN)
 
-# KV 엔진 확인 및 생성
+# KV 엔진 확인 및 생성, AppRole 발급
 if ! vault secrets list | grep -q '^transcendence/'; then
     vault secrets enable -path=transcendence kv
-fi
-
-# 초기 생성 단계에서만 AppRole발급
-if [ ! -f /vault-data/initapprole ]; then
-    # AppRole 생성 스크립트 실행
     vault auth enable approle
     sh backrole.sh && sh frontrole.sh
-    touch /vault-data/initapprole
 fi
 
 python3 secret.py
